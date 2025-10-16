1cbc87f7289656d1ce37626777cbf0fee00cbb87
OmaraFiFi0 <omarafifi846@gmail.com>
2025-10-16 11:51:40 +0300

    MVC03

--- GymManagementBLL/ViewModels/MemberViewModels/CreateMemberViewModel.cs
+++ GymManagementBLL/ViewModels/MemberViewModels/CreateMemberViewModel.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace GymManagementBLL.ViewModels.MemberViewModels
 {
     internal class CreateMemberViewModel
